624675f953fb16d823fa8d55d42955e4a6299382
Jose Emilio Cabana <jecabana@vanitymiami.com>
2014-11-13 09:56:11 -0500

    adding separate files

---
+++ fasteners.php
@@ -0,0 +1,57 @@
+<?
+	$page = "fasteners";
+	include('includes/header.php');
+?>
+		
+		<div id="content" class="no-sidebar">
+			<div id="title">
+				<div id="featured" class="type-3">
+					<img src="media/db/banner_Fasteners.jpg" alt="banner_Fasteners" width="980" height="330" />
+				</div>
+			</div>
+			
+			<div class="main">
+			<ul class="news">				
+					<li>
+						<h1 class="red mp0">FASTENERS</h1>
+                        <span class="hr"></span>
+
+						<h2>Standard & Custom Made:</h2>
+						<p>
+							We carry all types of standard hardware from machine screws to hex cap screw, washers and nuts in metric and american standards. We also specialize in custom made fasteners for the medical and commercial industries.					
+						</p>
+                        <span class="hr"></span>
+                        
+                        <h2>Construction Fasteners:</h2>
+						<p>
+							We offer a variety of construction hardware like anchors, heavy hex bolts, eye bolts, drywall screws, wood screws, rivets, tek screws, studs, washers, nuts and much more. We are your source for your construction fasteners needs.						
+						</p>
+                        <span class="hr"></span>
+                        
+                        <h2>Injection Molding:</h2>
+						<p>
+							With our partners, we assist in product design, prototype/production tooling, assembly and value added services. The facility is ISO 9001: 2008. We count on a full engineering & QC staff. The facility is equipped with 20 ton to 400 ton Nissei?s ready for any job. We specialize in medical molded product.				</p>
+                        <span class="hr"></span>
+                        
+                        <h2>Light assembly/Kitting:</h2>
+						<p>
+							We offer light assembly solutions like small 3 part harness production runs and small 4 part mechanical assembly work. We have dedicated personnel for this type of work, for when you need product fast and right. We also provide kitting from 2 parts to 30 parts to a kit.						
+						</p>
+                        <span class="hr"></span>
+                        
+                        <h2>Electrical/Mechanical production:</h2>
+						<p>
+							We supply high-end slides, bearings, terminal blocks and o-rings for industrial applications. We also concentrate on the electro mechanical side like, standoffs, terminals, lugs, liquid tight fittings and much more.						
+						</p>
+
+					</li>
+				</ul>
+				
+			</div>
+			
+		</div>
+		<!-- #content - end -->
+
+<?
+	include('includes/footer.php');
+?>
